ff4fe9155ff9caedb52c9f122cc850579fcacb55
Erofeev Sergey <erofeev28@gmail.com>
2016-10-31 20:51:14 +0600

    Улучшен код

--- lesson 2/task2.php
+++ lesson 2/task2.php
@@ -3,76 +3,50 @@
 /*
 1) Функция должна принимать 2 параметра:
         массив чисел;
-        строку, обозначающую арифметическое действие,    которое нужно выполнить со всеми элементами массива.
+        строку, обозначающую арифметическое действие, которое нужно выполнить со всеми элементами массива.
 2) Функция должна вывести результат на экран.
 3) Функция должна обрабатывать любой ввод, в том числе некорректный и выдавать сообщения об этом
 */
 
-$array = array(
-   mt_rand(1, 2),
-   mt_rand(1, 2),
-   mt_rand(1, 2),
-   mt_rand(1, 3)
-);
-$str = '/';
-
 function math($array, $operand = '+')
 {
+
     if (gettype($operand) !== 'string') {
         echo 'Ошибка: Операнд должен быть сторкой!';
         return false;
     }
     switch ($operand) {
         case '+':
-            for ($i = 0; $i < count($array); $i++) {
-                if ($i+1 < count($array)) {
-                    $str .=  $array[$i] . $operand;
-                } else {
-                    $str .=  $array[$i];
-                }
-                $answer += $array[$i];
+            $str = implode('+', $array);
+            foreach ($array as $val) {
+                $answer += $val;
             }
             break;
         case '-':
-            for ($i = 0; $i < count($array); $i++) {
-                if ($i+1 < count($array)) {
-                    $str .=  $array[$i] . $operand;
-                } else {
-                    $str .=  $array[$i];
+            $str = implode('-', $array);
+            foreach ($array as $key => $val) {
+                if ($key === 0) {
+                    $answer = $val;
+                    continue;
                 }
-                if ($i == 0) {
-                    $answer = $array[$i];
-                } else {
-                    $answer -= $array[$i];
-                }
+                $answer -= $val;
             }
             break;
         case '*':
-            for ($i = 0; $i < count($array); $i++) {
-                if ($i+1 < count($array)) {
-                    $str .=  $array[$i] . $operand;
-                } else {
-                    $str .=  $array[$i];
-                }
-                if ($i == 0) {
-                    $answer = $array[$i];
-                } else {
-                    $answer *= $array[$i];
-                }
+            $str = implode('*', $array);
+            $answer = 1;
+            foreach ($array as $key => $val) {
+                $answer *= $val;
             }
             break;
         case '/':
-            for ($i = 0; $i < count($array); $i++) {
-                if ($i+1 < count($array)) {
-                    $str .=  $array[$i] . $operand;
-                } else {
-                    $str .=  $array[$i];
+            $str = implode('/', $array);
+            foreach ($array as $key => $val) {
+                if ($key === 0) {
+                    $answer = $val;
+                    continue;
                 }
-                if ($i == 0) {
-                    $answer = $array[$i];
-                } else {
-                    $answer /= $array[$i];
-                }
+                $answer /= $val;
             }
             break;
         default:
@@ -82,4 +56,11 @@ function math($array, $operand = '+')
     echo $str . '=' . $answer;
 }
 
-math($array, $str);
+$array = array(
+    mt_rand(1, 2),
+    mt_rand(1, 2),
+    mt_rand(1, 2),
+    mt_rand(1, 3)
+);
+
+math($array, '/');
