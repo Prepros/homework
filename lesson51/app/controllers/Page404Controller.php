11ee7f00756afda411558f48045ef814e15342ef
Erofeev Sergey <erofeev28@gmail.com>
2016-11-24 12:18:18 +0600

    Домашняя работа 5 и 6

--- lesson51/app/controllers/Page404Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace app\core;
-
-class Page404Controller extends Controller
-{
-    public function actionIndex() {
-        echo "404";
-    }
-}
--- lesson51/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Отображение ошибок
-ini_set('display_errors', 1);
-
-session_start();
-
-// Запускатор
-require_once 'vendor/autoload.php';
-require_once 'app/app.php';
