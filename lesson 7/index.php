7cea5e0965d33fae00ebf41963747df38378c388
Erofeev Sergey <erofeev28@gmail.com>
2016-12-01 13:52:18 +0600

    Модернизация 7 задания, автоматическое заполнение таблицы данными

--- lesson 7/index.php
+++ lesson 7/index.php
@@ -7,20 +7,24 @@ require_once 'config.php';
 require_once 'Product.php';
 require_once 'Category.php';
 
-$product = new Product();
-$category = new Category();
 
+//$res = Capsule::table('categories')->where('name', $cat)->first();
+//print_r($res); exit;
 
 //***********
 // Миграция
 //***********
 
 // Удаление
-//Capsule::schema()->drop('products');
-//Capsule::schema()->drop('categories');
-//exit;
+// Если таблицы существую в БД, то удалить
+if (Capsule::schema()->hasTable('products')) {
+    Capsule::schema()->drop('products');
+}
+if (Capsule::schema()->hasTable('categories')) {
+    Capsule::schema()->drop('categories');
+}
 
-// Создание
+// Создание новые таблицы
 try {
     Capsule::schema()->create('categories', function ($table) {
         $table->increments('id');
@@ -40,3 +44,48 @@ try {
 } catch (Exception $e) {
     echo $e->getMessage();
 }
+
+// Входные данные
+$categories = array(
+    'Lenovo',
+    'HTC',
+    'Samsung'
+);
+
+$products = array(
+    'Lenovo' => array(
+        'Lenovo A2010',
+        'Lenovo Vibe B',
+        'Lenovo A916',
+        'Lenovo Vibe K5'
+    ),
+    'HTC' => array(
+        'HTC Desire 526G Dual Sim',
+        'HTC Desire 820G+',
+        'HTC Desire 628 Dual Sim'
+    ),
+    'Samsung' => array(
+        'Samsung Galaxy J5 SM-J500H/DS',
+        'Samsung Galaxy A5 SM-A500F'
+    )
+);
+
+// Заполняем таблицу категорий входными данными
+foreach ($categories as $cat) {
+    $category = new Category();
+    $category->name = $cat;
+    $category->save();
+}
+
+// Заполняем таблицу продуктов входными данными
+foreach ($products as $cat => $index) {
+    foreach ($index as $prod) {
+        $catobj = Capsule::table('categories')->select('id')->where('name', $cat)->first();
+        $product = new Product();
+        $product->name = $prod;
+        $product->category_id = $catobj->id;
+        $product->save();
+    }
+}
+
+echo "Таблицы созданы и заполнены";
